506f88fbc0e97e7b923f846c23818ec0fe91a071
Jasur <j.makhsudov@mail.ru>
2020-05-04 23:12:58 +0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        //$this->call(VauchersTableSeeder::class);
-        //$this->call(RolesTableSeeder::class);
-        //$this->call(CuisinesTableSeeder::class);
-        //$this->call(FoodTypeTableSeeder::class);
+        // $this->call(VauchersTableSeeder::class);
+        // $this->call(RolesTableSeeder::class);
+        // $this->call(CuisinesTableSeeder::class);
+        // $this->call(FoodTypeTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
